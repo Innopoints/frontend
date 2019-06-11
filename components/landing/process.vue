77b70c4aa3b8dc63287735ba362373e3bf71b905
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-06-11 14:18:21 +0200

    Add the process component that uses process-step

---
+++ components/landing/process.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="process">
+    <h2>How to get Innopoints</h2>
+    <div
+      v-for="(step, i) in steps"
+      :key="i"
+      class="step"
+    >
+      <Step v-bind="step" />
+    </div>
+  </section>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import Step from './process-step';
+  import steps from '../../constants/process';
+
+  export default Vue.extend({
+    components: {
+      Step,
+    },
+    data() {
+      return {
+        steps,
+      };
+    },
+  });
+</script>
