768072623be26b3fec5eb3aa2d43ec0ba6f1fc7c
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-06-11 20:12:48 +0200

    Wrap the backgroundColor prop in a computed value to prepare for it to be removed in the future Was not able to achieve this in a functional component

--- components/landing/store-card.vue
+++ components/landing/store-card.vue
@@ -1,19 +1,19 @@
-<template functional>
+<template>
   <div class="card shadow-2">
-    <img :src="props.img" :style="{background: props.backgroundColor}" />
+    <img :src="img" :style="{'background-color': background}" />
     <div
-      :style="{background: props.color}"
+      :style="{'background-color': color}"
       class="text-block"
     >
       <div class="title">
-        {{ props.title }}
+        {{ title }}
       </div>
       <span class="subtitle">
-        {{ props.subtitle }}
+        {{ subtitle }}
       </span>
       <a
-        :style="{color: props.color}"
-        :href="props.buttonURL"
+        :style="{'color': color}"
+        :href="buttonURL"
         class="btn filled white shadow-2"
       >view</a>
     </div>
@@ -27,11 +27,33 @@
       title: String,
       subtitle: String,
       color: String,
-      backgroundColor: String,
+      backgroundColor: String,      // to be removed
       buttonURL: {
         type: String,
         default: '#'
       },
     },
+    computed: {
+      background() {
+        return this.backgroundColor;  // remove this once the rest of the function is finalized
+        let color = this.color;
+        if(color.startsWith('#')) {
+          let r=0, g=0, b=0;
+          if(color.length === 7) {
+            r = parseInt(color.substring(1,3), 16);
+            g = parseInt(color.substring(3,5), 16);
+            b = parseInt(color.substring(5,7), 16);
+          } else if (color.length === 4) {
+            r = parseInt(color.charAt(1), 16);
+            g = parseInt(color.charAt(2), 16);
+            b = parseInt(color.charAt(3), 16);
+          } else {
+            throw `Unknown color string: ${color}`;
+          }
+          return `rgba(${r}, ${g}, ${b}, 0.15)`;
+        }
+        return undefined;
+      }
+    }
   };
 </script>
