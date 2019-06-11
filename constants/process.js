78e329a4803197cc3c87f8eb9bc8e3d75d73f8d0
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-06-11 14:18:57 +0200

    Add process constants

---
+++ constants/process.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: 'Volunteering',
+    subtitle: 'Innopolis University needs heroes like you to organize world-class events!',
+    img: 'images/landing/volunteering.png',
+    buttonLink: '#',
+    buttonText: 'See the events',
+  },
+  {
+    title: 'Event organization',
+    subtitle: 'A little bored from studies? Throw a party for the whole university to enjoy and have your efforts rewarded!',
+    img: 'images/landing/event-organizer.png',
+    buttonLink: '#',
+    buttonText: 'Propose an event',
+  },
+  {
+    title: 'Club leadership',
+    subtitle: 'A successful club needs a proactive leader. If you think that you’d make a good leader, pay a visit to our student clubs!',
+    img: 'images/landing/leader.png',
+    buttonLink: 'https://campuslife.innopolis.ru/clubs',
+    buttonText: 'See the clubs',
+  },
+  {
+    title: 'Student representative',
+    subtitle: 'Student clubs are not the only place to shine! Become the BDFL that your groupmates need!',
+    img: 'images/landing/representatives.png',
+    buttonLink: '#',
+    buttonText: 'Apply',
+  },
+];
