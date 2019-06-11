0833e62fa606120422514beff67585c28643d67f
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-06-11 14:17:48 +0200

    Add the Process Step component

---
+++ components/landing/process-step.vue
@@ -0,0 +1,25 @@
+<template functional>
+  <div class="container">
+    <img :src="props.img" />
+    <div class="description">
+      <h3 class="title">{{ props.title }}</h3>
+      <p class="subtitle">{{ props.subtitle }}</p>
+      <nuxt-link :to="props.buttonLink" class="btn filled green shadow-1">{{ props.buttonText }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      img: String,
+      title: String,
+      subtitle: String,
+      buttonLink: {
+        type: String,
+        default: '#',
+      },
+      buttonText: String,
+    },
+  };
+</script>
