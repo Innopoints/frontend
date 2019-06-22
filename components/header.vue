9fe057691e23e0331f288c41fd0a3cb97101eb90
VanishMax <maxkors2014@gmail.com>
2019-06-22 20:35:03 +0300

    Add simple vuex store with isAuth flag and toggle action

--- components/header.vue
+++ components/header.vue
@@ -5,19 +5,31 @@
         <img src="/images/browser/innou-icon-green.svg" />
         <span class="hide-mb">Innopoints</span>
       </div>
-      <nuxt-link class="btn outline green" to="#">sign in</nuxt-link>
+      <span
+        @click="toggleAuth"
+        v-ripple
+        class="btn outline green mdc-button"
+      >
+        sign {{ isAuth ? 'out' : 'in' }}
+      </span>
     </div>
   </header>
 </template>
 
 <script>
+  import {mapState, mapActions} from 'vuex';
+
   export default {
     name: "Header",
+    computed: mapState({
+      isAuth: state => state.auth.isAuth
+    }),
     methods: {
       resolution: () => {
         alert(`You current browser screen resolution.
         Width: ${window.innerWidth}px, height: ${window.innerHeight}px`);
-      }
+      },
+      ...mapActions({toggleAuth: 'auth/toggleAuth'}),
     }
   };
 </script>
---
+++ store/auth.js
@@ -0,0 +1,17 @@
+export default {
+  state() {
+    return {
+      isAuth: false
+    };
+  },
+  mutations: {
+    changeAuth(state, value) {
+      state.isAuth = value;
+    }
+  },
+  actions: {
+    toggleAuth({commit, state}) {
+      commit('changeAuth', !state.isAuth);
+    }
+  }
+};
