0b641f4caa200c55c10ad050232a09646b1295ac
VanishMax <maxkors2014@gmail.com>
2019-06-22 20:35:03 +0300

    Add simple vuex store with isAuth flag and toggle action

--- components/landing/header.vue
+++ components/landing/header.vue
@@ -1,18 +1,30 @@
 <template>
   <header class="container landing">
     <img @click="resolution" src="/images/landing/innou-logo-text.png" class="logo" />
-    <nuxt-link v-ripple to="/" class="btn outline green mdc-button">sign in</nuxt-link>
+    <span
+      @click="toggleAuth"
+      v-ripple
+      class="btn outline green mdc-button"
+    >
+      sign {{ isAuth ? 'out' : 'in' }}
+    </span>
   </header>
 </template>
 
 <script>
+  import {mapState, mapActions} from 'vuex';
+
   export default {
     name: "LandingHeader",
+    computed: mapState({
+      isAuth: state => state.auth.isAuth
+    }),
     methods: {
       resolution: () => {
         alert(`You current browser screen resolution.
         Width: ${window.innerWidth}px, height: ${window.innerHeight}px`);
-      }
+      },
+      ...mapActions({toggleAuth: 'auth/toggleAuth'}),
     }
   };
 </script>
