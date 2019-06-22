2f0c1279357732822a5807e77f3728aa4090812e
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-06-22 16:34:28 +0200

    Add sample data for events

---
+++ constants/events/events.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    img: '/images/browser/chess-club.png',
+    title: 'Chess Tournament',
+    date: 'Jan 21 – Feb 24, 2019',
+    organizer: '319',
+    activities: [
+      'Requisite management',
+      'Catering',
+      'Script writing',
+      'Judging',
+      'Preparation',
+    ],
+    url: '#',
+  },
+  {
+    img: '/images/browser/image.png',
+    title: 'All-Russian Robotics Olympiad',
+    date: 'Jan 21 – Feb 24, 2019',
+    organizer: 'Olympiads dept.',
+    activities: [
+      'Requisite management',
+      'Catering',
+    ],
+    url: '#',
+  },
+  {
+    img: '/images/browser/chess-club.png',
+    title: 'Chess Tournament',
+    date: 'Jan 21 – Feb 24, 2019',
+    organizer: '319',
+    activities: [
+      'Requisite management',
+      'Catering',
+      'Script writing',
+      'Judging',
+      'Preparation',
+    ],
+    url: '#',
+  },
+];
