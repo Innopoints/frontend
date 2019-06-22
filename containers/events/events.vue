b66bd5339d17d651916073c7fddd195404658558
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-06-22 16:33:54 +0200

    Add the events container

---
+++ containers/events/events.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="events">
+    <h1>Ongoing events</h1>
+    <!-- Filters here -->
+    <div class="cards">
+      <Event
+        v-for="(event, i) in events"
+        :key="i"
+        v-bind="event"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import Event from '../../components/events/event';
+  import events from '../../constants/events/events';
+  export default Vue.extend({
+    components: {
+      Event,
+    },
+    data() {
+      return {
+        events,
+      };
+    },
+  });
+</script>
