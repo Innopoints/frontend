e24836209b02ee9c3b504054786a5e5aadbc8caf
VanishMax <maxkors2014@gmail.com>
2019-07-10 20:14:31 +0500

    Add button and card ui components

---
+++ components/ui/button.vue
@@ -0,0 +1,71 @@
+<template>
+  <!--eslint-disable-next-line-->
+  <component
+    :is="link ? 'a' : 'button'"
+    :class="['btn', filled && 'filled', outline && 'outline', danger && 'danger', round && 'round', normal && 'normal']"
+    v-if="link"
+    :href="href"
+    v-else
+    :disabled="disabled"
+  >
+    <template v-if="badge">
+      <div class="badge">
+        <img :src="img" class="mr" alt="" />
+      </div>
+    </template>
+
+    <template v-else>
+      <img :src="img" class="mr" alt="" />
+    </template>
+
+    <slot />
+  </component>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      link: {
+        type: Boolean,
+        default: false
+      },
+      href: {
+        type: String,
+        default: '#'
+      },
+      img: {
+        type: String,
+        default: null
+      },
+      filled: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      outline: {
+        type: Boolean,
+        default: false
+      },
+      danger: {
+        type: Boolean,
+        default: false
+      },
+      badge: {
+        type: Boolean,
+        default: false
+      },
+      round: {
+        type: Boolean,
+        default: false
+      },
+      normal: {
+        type: Boolean,
+        default: false
+      },
+    }
+  };
+</script>
---
+++ components/ui/card.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card'
+  };
+</script>
