535f420a68a7174f013f68dbd477790dc1e4bb59
VanishMax <maxkors2014@gmail.com>
2019-07-11 01:44:10 +0500

    End buttons showcase and component

--- pages/ui.vue
+++ pages/ui.vue
@@ -1,164 +1,15 @@
 <template>
   <div class="body-wrap">
-    <card>
-      <h1>Buttons</h1>
-      <div class="showcase">
-        <Button
-          :click="alert"
-          label="simple"
-        />
-
-        <Button
-          :click="alert"
-          label="danger"
-          danger
-        />
-
-        <Button
-          :click="alert"
-          label="filled"
-          filled
-        />
-
-        <Button
-          :click="alert"
-          label="outline"
-          outline
-        />
-        <Button
-          :click="alert"
-          label="filled danger"
-          filled
-          danger
-        />
-
-        <Button
-          :click="alert"
-          label="outline danger"
-          outline
-          danger
-        />
-
-        <Button
-          :click="alert"
-          label="outline disable"
-          outline
-          disabled
-        />
-      </div>
-      <hr />
-
-      <h1>Button-Links</h1>
-      <div class="showcase">
-        <Button
-          link
-          href="/"
-          label="simple"
-        />
-
-        <Button
-          link
-          away
-          href="http://example.com"
-          label="danger"
-          danger
-        />
-
-        <Button
-          link
-          href="#"
-          label="filled"
-          filled
-        />
-
-        <Button
-          link
-          href="#"
-          label="outline"
-          outline
-        />
-
-        <Button
-          link
-          href="#"
-          label="filled danger"
-          filled
-          danger
-        />
-
-        <Button
-          link
-          href="#"
-          label="filled danger"
-          outline
-          danger
-        />
-      </div>
-      <hr />
-
-      <h1>With icons</h1>
-      <div class="showcase">
-        <Button
-          round
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          normal
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          badge
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          outline
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          filled
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          label="with label"
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          filled
-          label="with label"
-          img="/images/events/calendar.svg"
-        />
-
-        <Button
-          round
-          outline
-          label="with label"
-          img="/images/events/calendar.svg"
-        />
-      </div>
-    </card>
+    <ButtonsShowcase />
   </div>
 </template>
 
 <script>
-  import Card from '../components/ui/card';
-  import Button from '../components/ui/button';
+  import ButtonsShowcase from '../containers/ui/buttons';
 
   export default {
     components: {
-      Button,
-      Card,
+      ButtonsShowcase
     },
     head: {
       title: 'UI kit',
@@ -166,11 +17,6 @@
         { hid: 'description', name: 'description', content: `UI kit for innopoints project` }
       ]
     },
-    methods: {
-      alert() {
-        alert('Some text for the show');
-      }
-    }
   };
 </script>
 
