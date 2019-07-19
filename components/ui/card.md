aadc23d5e014a8f2dc17c4bcefe51db57050d4cc
VanishMax <maxkors2014@gmail.com>
2019-07-19 14:14:50 +0300

    Improve card ui-component

---
+++ components/ui/card.md
@@ -0,0 +1,7 @@
+## CARD
+
+Block with the shadow and the padding. Can accept image
+
+Property | Default | Description
+--- | --- | ---
+img | `''` | Image to show at the top of the card
--- components/ui/card.vue
+++ components/ui/card.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="card">
-    <slot />
+  <div :class="img ? 'with-image' : ''" class="card">
+    <img v-if="img" src="img" />
+    <div v-if="img" class="content">
+      <slot />
+    </div>
+    <template v-else>
+      <slot />
+    </template>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Card'
+    name: 'Card',
+    props: {
+      img: {
+        type: String,
+        default: ''
+      }
+    }
   };
 </script>
