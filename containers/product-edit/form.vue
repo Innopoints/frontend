1ed95a75fdce751abc9a62309f93d6e62aaa684d
VanishMax <maxkors2014@gmail.com>
2019-07-21 17:17:00 +0300

    Add color-picker dropdown and the ability to choose the size

--- containers/product-edit/form.vue
+++ containers/product-edit/form.vue
@@ -36,13 +36,100 @@
     </FormField>
 
     <header class="varieties">
-      <h2>varieties</h2>
+      <h2>Varieties</h2>
       <Toggle
         :checked="inSizes"
         :click="toggleSizes"
         label="the product comes in sizes"
       />
     </header>
+
+    <ul>
+      <li class="variety">
+        <div class="settings">
+          <Dropdown :chevron="false">
+            <template v-slot:opener>
+              choose color
+              <div class="selected-color" style="background-color: blueviolet" />
+            </template>
+            <template>
+              kill me
+            </template>
+          </Dropdown>
+
+          <div v-if="!inSizes" class="quantity">
+            <label for="quantity">Quantity</label>
+            <TextField
+              id="quantity"
+              type="number"
+            />
+          </div>
+        </div>
+
+
+        <div v-if="inSizes" class="sizes-wrapper">
+          <label class="label">Sizes</label>
+          <div class="sizes">
+            <TextField
+              item="text"
+              text="XS"
+              type="number"
+              class="right-align"
+              placeholder="0"
+            />
+            <TextField
+              item="text"
+              text="S"
+              type="number"
+              class="right-align"
+              placeholder="0"
+            />
+            <TextField
+              item="text"
+              text="M"
+              type="number"
+              class="right-align"
+              placeholder="0"
+            />
+            <TextField
+              item="text"
+              text="L"
+              type="number"
+              class="right-align"
+              placeholder="0"
+            />
+            <TextField
+              item="text"
+              text="XL"
+              type="number"
+              class="right-align"
+              placeholder="0"
+            />
+            <TextField
+              item="text"
+              text="XXL"
+              type="number"
+              class="right-align"
+              placeholder="0"
+            />
+          </div>
+        </div>
+
+        <div class="image-platform">
+          <input
+            id="file-input1"
+            type="file"
+            multiple
+            accept="image/*"
+          />
+          <img class="icon mr" src="/images/create-product/image.svg" />
+          <label class="title" for="file-input1">
+            drag &amp; drop here or click to upload photos
+          </label>
+          <small>Make sure the ratio is 1:1 (square)</small>
+        </div>
+      </li>
+    </ul>
   </form>
 </template>
 
@@ -50,10 +137,12 @@
   import FormField from '../../components/ui/form-field';
   import TextField from '../../components/ui/text-field';
   import Toggle from '../../components/ui/toggle';
+  import Dropdown from '../../components/ui/dropdown';
 
   export default {
     name: 'ProductEditForm',
     components: {
+      Dropdown,
       Toggle,
       TextField,
       FormField
