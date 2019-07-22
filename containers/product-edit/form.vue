c8e82bef369c7780d638868a6d7bfd865226c8c3
VanishMax <maxkors2014@gmail.com>
2019-07-22 14:50:03 +0300

    Transfer markup for varieties

--- containers/product-edit/form.vue
+++ containers/product-edit/form.vue
@@ -45,89 +45,15 @@
     </header>
 
     <ul>
-      <li class="variety">
-        <div class="settings">
-          <Dropdown :chevron="false">
-            <template v-slot:opener>
-              choose color
-              <div class="selected-color" style="background-color: blueviolet" />
-            </template>
-            <template>
-              kill me
-            </template>
-          </Dropdown>
+      <ProductFormVariety
+        v-for="i in vars"
+        :key="i"
+        :removable="i !== 1"
+        :in-sizes="inSizes"
+      />
 
-          <div v-if="!inSizes" class="quantity">
-            <label for="quantity">Quantity</label>
-            <TextField
-              id="quantity"
-              type="number"
-            />
-          </div>
-        </div>
-
-
-        <div v-if="inSizes" class="sizes-wrapper">
-          <label class="label">Sizes</label>
-          <div class="sizes">
-            <TextField
-              item="text"
-              text="XS"
-              type="number"
-              class="right-align"
-              placeholder="0"
-            />
-            <TextField
-              item="text"
-              text="S"
-              type="number"
-              class="right-align"
-              placeholder="0"
-            />
-            <TextField
-              item="text"
-              text="M"
-              type="number"
-              class="right-align"
-              placeholder="0"
-            />
-            <TextField
-              item="text"
-              text="L"
-              type="number"
-              class="right-align"
-              placeholder="0"
-            />
-            <TextField
-              item="text"
-              text="XL"
-              type="number"
-              class="right-align"
-              placeholder="0"
-            />
-            <TextField
-              item="text"
-              text="XXL"
-              type="number"
-              class="right-align"
-              placeholder="0"
-            />
-          </div>
-        </div>
-
-        <Dropzone
-          id="dropzone"
-          :options="options"
-          :destroyDropzone="false"
-          :useCustomSlot="true"
-          class="image-platform"
-        >
-          <img class="icon mr" src="/images/create-product/image.svg" />
-          <label class="title" for="file-input1">
-            drag &amp; drop here or click to upload photos
-          </label>
-          <small>Make sure the ratio is 1:1 (square)</small>
-        </Dropzone>
+      <li class="more">
+        <Button img="/images/create-product/plus.svg" label="add another variety" />
       </li>
     </ul>
   </form>
@@ -137,24 +63,22 @@
   import FormField from '../../components/ui/form-field';
   import TextField from '../../components/ui/text-field';
   import Toggle from '../../components/ui/toggle';
-  import Dropdown from '../../components/ui/dropdown';
-  import Dropzone from 'nuxt-dropzone';
+  import ProductFormVariety from './variety';
+  import Button from '../../components/ui/button';
 
   export default {
     name: 'ProductEditForm',
     components: {
-      Dropdown,
+      Button,
+      ProductFormVariety,
       Toggle,
       TextField,
       FormField,
-      Dropzone
     },
     data() {
       return {
         inSizes: false,
-        options: {
-          url: "http://httpbin.org/anything"
-        }
+        vars: 2
       };
     },
     methods: {
