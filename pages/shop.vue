5b0028de5590072785a6e0c8b06ce1834a767a8d
dark0n3 <dark.sonik1998@gmail.com>
2019-07-30 17:42:22 +0300

    Add InnoStore tagline and bottom link

--- pages/shop.vue
+++ pages/shop.vue
@@ -1,23 +1,31 @@
 <template>
-  <section class="shop padded">
-    <div class="cards">
-      <Card
-        v-for="(item, i) in items"
-        :key="i"
-        v-bind="item"
-        priced
-      />
-    </div>
-  </section>
+  <div id="store" class="material">
+    <ShopTagline />
+    <section class="shop padded">
+      <div class="cards">
+        <Card
+          v-for="(item, i) in items"
+          :key="i"
+          v-bind="item"
+          priced
+        />
+      </div>
+    </section>
+    <p class="link-bottom padded">
+      What’s so cool about being a volunteer? The <nuxt-link to="/shop">InnoStore</nuxt-link> has your answers!
+    </p>
+  </div>
 </template>
 
 <script>
   import Card from '../components/landing/store-card';
+  import ShopTagline from '../components/shop/tagline';
   import items from '../constants/shop/store';
 
   export default {
     components: {
       Card,
+      ShopTagline
     },
     data() {
       return {
