32028e0ac8d16576e7b0750d64fc5bc5c82506ae
dark0n3 <dark.sonik1998@gmail.com>
2019-07-30 11:36:05 +0300

    Add constants(products) for shop

---
+++ constants/shop/store.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    img: '/images/landing/sweatshirt.png',
+    title: 'Write clean code',
+    subtitle: 'Sweatshirt',
+    color: '#fff0f0',
+    buttonURL: '#',
+    price: 1337,
+    variations: ['red', 'green']
+  },
+  {
+    img: '/images/landing/tshirt.png',
+    title: 'ASCII Whale',
+    subtitle: 'T-Shirt',
+    color: '#f5fdff',
+    buttonURL: '#',
+    price: 1337
+  },
+  {
+    img: '/images/landing/umbrella.png',
+    title: 'IU Umbrella',
+    subtitle: '',
+    color: '#f6f6f6',
+    buttonURL: '#',
+    price: 1337
+  },
+  {
+    img: '/images/landing/sweatshirt.png',
+    title: 'Write clean code',
+    subtitle: 'Sweatshirt',
+    color: '#fff0f0',
+    buttonURL: '#',
+    price: 1337,
+    variations: ['red', 'green']
+  },
+  {
+    img: '/images/landing/tshirt.png',
+    title: 'ASCII Whale',
+    subtitle: 'T-Shirt',
+    color: '#f5fdff',
+    buttonURL: '#',
+    price: 1337
+  },
+  {
+    img: '/images/landing/umbrella.png',
+    title: 'IU Umbrella',
+    subtitle: '',
+    color: '#f6f6f6',
+    buttonURL: '#',
+    price: 1337
+  },
+];
