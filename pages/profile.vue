77837b49d6cb6573e2407118d245344d84805d34
VanishMax <maxkors2014@gmail.com>
2019-07-31 11:54:15 +0300

    Imprort profile assets

---
+++ pages/profile.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Profile',
+      meta: [
+        { hid: 'description', name: 'description', content: `Your innopoints profile` },
+      ],
+    },
+  };
+</script>
+
+<style lang="scss" src="../static/css/profile/index.scss" />
