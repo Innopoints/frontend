8eb93a7df16c44a68227196636c80c3802f3f057
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-08-05 22:22:50 +0200

    Sync data with Vuex

--- containers/product-edit/preview.vue
+++ containers/product-edit/preview.vue
@@ -2,12 +2,37 @@
   <section class="preview">
     <h2>Preview</h2>
     <Card img="/images/create-product/placeholder.svg">
-      <span class="name placeholder" />
+      <span :class="name ? 'title' : 'placeholder'" class="name">{{ name }}</span>
+      <span v-show="type" class="type subtitle">{{ type }}</span>
+      <Labelled v-show="description" label="Description">
+        {{ description }}
+      </Labelled>
+      <div v-show="varietiesWithColor.length > 0" class="card-row top">
+        <Labelled label="Colors">
+          <RadioGroup
+            :items="varietiesWithColor"
+            v-model="selectedVariety"
+            name="preview"
+            horizontal
+          />
+        </Labelled>
+        <Labelled v-if="selectedVariety" label="Sizes">
+          <Chip
+            v-for="(amount, size) in selectedVariety.sizes"
+            :key="size"
+            :disabled="amount <= 0"
+            :text="size"
+            name="previewSizes"
+            class="size"
+            small
+          />
+        </Labelled>
+      </div>
       <div class="card-row">
         <div class="labeled text">
           <span class="label">Price</span>
           <span class="content">
-            <span class="price placeholder" />
+            <span :class="{placeholder: price <= 0}" class="price">{{ price > 0 ? price : '' }}</span>
             <img src="/images/create-product/innopoint.svg" class="ml" />
           </span>
         </div>
@@ -18,18 +43,46 @@
       </div>
     </Card>
     <div class="actions">
-      <Button danger label="clear fields" />
+      <Button @click="$store.commit('newProduct/reset')" danger label="clear fields" />
       <Button filled label="create product" />
     </div>
   </section>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import Card from '../../components/ui/card';
   import Button from '../../components/ui/button';
+  import Labelled from '../../components/ui/labeled';
+  import RadioGroup from '../../components/ui/radio-group';
+  import Chip from '../../components/ui/chip';
 
   export default {
     name: 'ProductEditPreview',
-    components: { Button, Card },
+    components: {
+      Button,
+      Card,
+      Labelled,
+      RadioGroup,
+      Chip,
+    },
+    data() {
+      return {
+        selectedVariety: null,
+      };
+    },
+    computed: {
+      ...mapState('newProduct', [
+        'name',
+        'price',
+        'description',
+        'type',
+        'varieties',
+      ]),
+      varietiesWithColor() {
+        return this.varieties.filter(v => v.color);
+      },
+    },
+    
   };
 </script>
