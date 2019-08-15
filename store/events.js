4b90edd3e763367a04236cab0d76caa3f2c61e7e
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-08-15 15:32:14 +0300

    Edit single competence instead of  entire array

--- store/events.js
+++ store/events.js
@@ -22,6 +22,9 @@ export default {
     changeField(state, value) {
       state[value.type] = value.value;
     },
+    changeCompetence(state, payload) {
+      state.filters.competences[payload.index].checked = payload.checked;
+    },
   },
   actions: {
     changeSearch({commit}, value) {
@@ -31,13 +34,12 @@ export default {
       commit('changeFilter', {type: 'spots', value: value});
     },
     changeCompetence({commit, state}, value) {
-      commit('changeFilter', {type: 'competences', value: state.filters.competences.map(x => {
-          if(x.name === value.name) return {name: x.name, checked: !value.checked};
-          return x;
-        })});
+      const index = state.filters.competences.findIndex(c => c.name == value.name);
+      if(index === -1) return;
+      commit('changeCompetence', {index, checked: value.checked});
     },
     clearCompetences({commit, state}) {
-      commit('changeFilter', {type: 'competences', value: state.filters.competences.map(x => ({name: x.name, checked: false})) });
+      state.filters.competences.forEach((c, index) => commit('changeCompetence', {index, checked: false}));
     },
   },
   getters: {
