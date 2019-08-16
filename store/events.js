ffcca4a4712b1c654e7ed62a24f3113d6c1c8160
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-08-16 14:26:06 +0300

    Filter and order the events in the getter

--- store/events.js
+++ store/events.js
@@ -12,7 +12,8 @@ export default {
         startDate: null,
         endDate: null,
       },
-      events: events,
+      orderBy: '',
+      events,
     };
   },
   mutations: {
@@ -44,7 +45,39 @@ export default {
   },
   getters: {
     events: state => {
-      return state.events.filter(event => state.filters.spots <= event.spots);
+      const filters = state.filters;
+      const excludedCompetences = filters.competences
+          .filter(comp => comp.checked === false)
+          .map(comp => comp.name);
+      let events = state.events
+          .filter(event => event.title.toLocaleLowerCase().includes(filters.search))
+          .filter(event => filters.spots <= event.spots)
+          .filter(event => !event.competences.every(c => excludedCompetences.includes(c)));
+      
+      if(filters.startDate) {
+        events = events.filter(event => event.date.start >= filters.startDate);
+      }
+      if(filters.endDate) {
+        events = events.filter(event => event.date.end <= filters.endDate);
+      }
+      if(state.orderBy) {
+        events.sort((event1, event2) => {
+          switch(state.orderBy) {
+            case 'most recent':
+              return event2.date.start - event1.date.start;
+            case 'least recent':
+              return event1.date.start - event2.date.start;
+            case 'most valuable':
+              return event2.points - event1.points;   // TODO: confirm property name
+            case 'least valuable':
+              return event1.points - event2.points;   // TODO: same ^^
+            default:
+              return 0;
+          }
+        });
+      }
+
+      return events;
     },
   },
 };
