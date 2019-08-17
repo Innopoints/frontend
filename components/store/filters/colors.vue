4bf8b7e56985aae99000c72b8eddf8edb96a3d98
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-08-17 13:57:16 +0300

    Create colors and price filtering components

---
+++ components/store/filters/colors.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="filter colors">
+    <span class="name">colors</span>
+    <CheckboxGroup
+      :value="colors"
+      @change="toggle"
+      class="color-grid"
+      round
+      colored
+    />
+  </li>
+</template>
+
+<script>
+  import CheckboxGroup from '@/components/ui/checkbox-group';
+  
+  export default {
+    components: {
+      CheckboxGroup,
+    },
+    computed: {
+      colors() {
+        return this.$store.state.store.filters.colors;
+      },
+    },
+    methods: {
+      toggle({color}) {
+        this.$store.commit('store/toggleColor', color);
+      },
+    },
+  };
+</script>
