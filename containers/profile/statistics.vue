f1b40a28539b8e811a690de5b835209acfba8fe3
VanishMax <maxkors2014@gmail.com>
2019-08-31 22:46:34 +0300

    Transfer new statistics markup

--- containers/profile/statistics.vue
+++ containers/profile/statistics.vue
@@ -1,60 +1,75 @@
 <template>
-  <section class="statistics padded">
-    <header>
-      <h2 class="btn-align">Statistics</h2>
-      <Dropdown class="btn-shift">
+  <section class="statistics">
+    <div class="period">
+      <span class="hide-tb">Showing statistics</span>
+      <Dropdown>
         <template v-slot:label>
           over the past 3 months
         </template>
-        <template>
-          When you see it, run!
-        </template>
+        <span class="label">Period of statistics</span>
+        <ul class="mt">
+          <li>
+            <Button label="last week" />
+          </li>
+          <li>
+            <Button label="last month" />
+          </li>
+          <li>
+            <Button label="last 3 month" />
+          </li>
+          <li>
+            <Button label="last semester" />
+          </li>
+          <li>
+            <Button label="last year" />
+          </li>
+          <li>
+            <Button label="all time" />
+          </li>
+        </ul>
+        <hr data-text="or" />
+        <Button label="select date range" img="/images/icons/calendar.svg" />
       </Dropdown>
-    </header>
+    </div>
 
-    <div>
-      <div class="top-cards">
-        <Card class="volunteering">
-          <header>
-            <Clock />
-            {{ hours }} volunteering hours
-          </header>
-          <div class="details">15 volunteering positions handled</div>
-          <div class="actions">
-            <Button label="export volunteering data" />
-          </div>
-        </Card>
-        <Card class="rating">
-          <header>
-            <Rating />
-            Volunteer rating
-          </header>
-          <div class="content">
-            <Star class="star" />
-            <Star class="star" />
-            <Star class="star" />
-            <Star class="star" />
-            <Star class="star" />
-          </div>
-          <div class="details">average rating of 15 people</div>
-        </Card>
-      </div>
+    <Button img="/images/icons/file-text.svg" label="create a volunteer report" />
 
-      <Card class="competences">
+    <div class="top-stats">
+      <div class="stat">
         <header>
-          <Stats />
-          Developed competences
+          <Clock class="icon" />
+          {{ hours }} volunteering hours
         </header>
-        <div class="details">
-          click a competence to find opportunities for development
-        </div>
+        <div class="details">15 volunteering positions handled</div>
+      </div>
+      <div class="stat">
+        <header>
+          <Star class="icon" />
+          Volunteer rating
+          <span class="rating">{{ rating }}</span>
+        </header>
+        <Button label="view feedback">
+          view feedback<Dot />
+        </Button>
+      </div>
+    </div>
+
+    <div class="stat competences">
+      <header>
+        <Stats class="icon" />
+        Developed competences
+      </header>
+      <figure>
         <div class="bar-chart" />
         <ul class="legend">
-          <li><Button label="proactivity" /></li>
-          <li><Button label="critical thinking" /></li>
-          <li><Button label="communication" /></li>
+          <li>
+            <Button link href="#" label="proactivity">
+              <div class="icon" style="background: rgb(237, 137, 137);" />
+              proactivity
+            </Button>
+          </li>
         </ul>
-      </Card>
+      </figure>
     </div>
   </section>
 </template>
@@ -62,28 +77,33 @@
 <script>
   import {mapState} from 'vuex';
   import Dropdown from '../../components/ui/dropdown';
-  import Card from '../../components/ui/card';
   import Clock from '../../static/images/icons/clock.svg';
-  import Rating from '../../static/images/icons/thumbs-up.svg';
   import Star from '../../static/images/icons/star.svg';
   import Stats from '../../static/images/icons/bar-chart-2.svg';
   import Button from '../../components/ui/button';
+  import Dot from '@/components/ui/dot';
 
   export default {
     name: 'ProfileStatistics',
     components: {
+      Dot,
       Button,
-      Card,
       Dropdown,
       Clock,
-      Rating,
       Star,
       Stats,
     },
     computed: {
       ...mapState({
         hours: state => state.user.volunteeringHours,
+        rating: state => state.user.rating,
       }),
     },
   };
 </script>
+
+<style>
+  .statistics {
+    display: block !important;
+  }
+</style>
