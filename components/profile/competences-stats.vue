cc382aeda621640175b449daf29517d4a0d4ef95
VanishMax <maxkors2014@gmail.com>
2019-08-31 23:39:55 +0300

    Add legend buttons for stats

--- components/profile/competences-stats.vue
+++ components/profile/competences-stats.vue
@@ -7,12 +7,21 @@
     <figure>
       <div class="bar-chart" />
       <ul class="legend">
-        <li>
-          <Button link href="#" label="proactivity">
-            <div class="icon" style="background: rgb(237, 137, 137);" />
-            proactivity
+        <li
+          v-for="stat in stats"
+          :key="stat.id"
+        >
+          <Button
+            @click="goTo(stat.id)"
+            :label="stat.name"
+            href="/projects"
+            link
+          >
+            <div :style="'background: ' + stat.color" class="icon" />
+            {{ stat.name }}
           </Button>
         </li>
+        <p>click a competence to find opportunities for development</p>
       </ul>
     </figure>
   </div>
@@ -21,9 +30,20 @@
 <script>
   import Button from '@/components/ui/button';
   import Stats from '../../static/images/icons/bar-chart-2.svg';
+  import stats from '@/constants/profile/stats-competences';
 
   export default {
     name: 'CompetencesStats',
     components: { Button, Stats },
+    data() {
+      return {
+        stats,
+      };
+    },
+    methods: {
+      goTo(id) {
+        console.log(id);
+      },
+    },
   };
 </script>
