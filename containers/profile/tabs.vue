ebdd08804bbe7b8189d03dd09b0f9fc73bdc3a4b
VanishMax <maxkors2014@gmail.com>
2019-08-31 18:39:30 +0300

    Implement tabs component

---
+++ containers/profile/tabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="tabs">
+    <label @click="$emit('change', 'T')" class="tab">
+      <input
+        type="radio"
+        name="tabs"
+        value="timeline"
+        checked
+      />
+      <div class="icon">Timeline</div>
+    </label>
+    <label @click="$emit('change', 'S')" class="tab">
+      <input type="radio" name="tabs" value="statistics" />
+      <div class="icon">Statistics</div>
+    </label>
+    <label @click="$emit('change', 'N')" class="tab">
+      <input type="radio" name="tabs" value="notifications" />
+      <div class="icon">Notifications</div>
+    </label>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'ProfileTabs',
+  };
+</script>
