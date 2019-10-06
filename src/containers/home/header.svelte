d5acd31ac267c3552f6bbe21704deaac873c852e
VanishMax <maxkors2014@gmail.com>
2019-10-06 03:51:17 +0300

    Add basic store-authentication

--- src/containers/home/header.svelte
+++ src/containers/home/header.svelte
@@ -1,5 +1,11 @@
 <script>
   import Button from 'ui/button.svelte';
+  import user, { logIn } from '@/store/user';
+  let isAuthed;
+
+  user.subscribe(val => {
+    isAuthed = val.isAuthed;
+  });
 </script>
 
 <header class="container">
@@ -7,5 +13,11 @@
     src="images/innou-logo.svg"
     class="logo"
     alt="Innopolis University logo" />
-  <Button href="#" outline>sign in</Button>
+  {#if !isAuthed}
+    <Button outline on:click={logIn}>sign in</Button>
+  {:else}
+    <Button href="/profile" round>
+      <svg src="images/icons/user.svg" />
+    </Button>
+  {/if}
 </header>
