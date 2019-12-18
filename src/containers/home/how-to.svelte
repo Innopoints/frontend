f818abd859063fc493fa4ed2992f3af765f2faf1
VanishMax <maxkors2014@gmail.com>
2019-12-18 17:25:33 +0300

    Change how-to container in home page

--- src/containers/home/how-to.svelte
+++ src/containers/home/how-to.svelte
@@ -1,16 +1,30 @@
 <script>
-  import HowToCard from '@/components/home/how-to-card.svelte';
-  import howTos from '@/constants/home/how-tos';
+  import Card from 'ui/card.svelte';
 </script>
 
 <section class="how-to">
   <h2>How does it work?</h2>
   <div class="container">
-    {#each howTos as howTo, i (howTo.title)}
-      <HowToCard {...howTo} />
-      {#if i !== 2}
-        <svg src="images/icons/arrow-down.svg" class="arrow" />
-      {/if}
-    {/each}
+    <Card>
+      <svg src="/images/icons/zap.svg" class="icon" />
+      <span class="title">Be proactive</span>
+      <span class="subtitle">Participate in event organization, lead student clubs, do other cool things</span>
+    </Card>
+
+    <svg src="images/icons/arrow-down.svg" class="arrow" />
+
+    <Card>
+      <svg src="/images/icons/eye.svg" class="icon" />
+      <span class="title">Reflect</span>
+      <span class="subtitle">Analyze your work and identify the progress and areas for improvement</span>
+    </Card>
+
+    <svg src="images/icons/arrow-down.svg" class="arrow" />
+
+    <Card>
+      <svg src="/images/icons/award.svg" class="icon" />
+      <span class="title">Get cool rewards!</span>
+      <span class="subtitle">Receive innopoints to spend on cool merch of Innopolis University</span>
+    </Card>
   </div>
 </section>
