180817723daad4d57a25f37b0357f078ffeefd96
VanishMax <maxkors2014@gmail.com>
2020-01-03 13:49:11 +0300

    Send comment and open user profiles in admin dashboard

--- src/components/dashboard/see-profile.svelte
+++ src/components/dashboard/see-profile.svelte
@@ -1,10 +1,21 @@
+<script>
+  import Button from 'ui/button.svelte';
+
+  export let users = [];
+  const open = () => {
+    for (let user of users) {
+      window.open('/profile?user=' + user.email);
+    }
+  };
+</script>
+
 <div class="see-profile">
   <header>
     <svg src="/images/icons/user.svg" class="icon mr-2" />
     See their profiles
   </header>
   <div class="subtitle">This will open new tabs.</div>
-  <a href="/profile?user=some" target="_blank" class="btn">
+  <Button on:click={open} disabled={!users.length}>
     open profile
-  </a>
+  </Button>
 </div>
--- src/components/dashboard/send-comment.svelte
+++ src/components/dashboard/send-comment.svelte
@@ -1,6 +1,18 @@
 <script>
   import TextField from 'ui/text-field.svelte';
   import Button from 'ui/button.svelte';
+
+  export let users = [];
+  $: {
+    if (users.length) success = false;
+  }
+  let success = false;
+  let text = null;
+
+  const send = () => {
+    text = null;
+    success = true;
+  };
 </script>
 
 <div class="send-message">
@@ -9,13 +21,20 @@
     Send a message
   </header>
   <form>
-    <TextField multiline cols="5" />
+    <TextField
+      bind:value={text}
+      multiline
+      cols="5"
+      on:input={() => success = false}
+    />
     <div class="actions">
-      <Button>
+      <Button disabled={!text} on:click={send}>
         <svg src="/images/icons/send.svg" class="icon mr" />
         send
       </Button>
-      <span class="status good" style="display: none;">success!</span>
+      {#if success}
+        <span class="status good">success!</span>
+      {/if}
       <span class="status bad" style="display: none;">failed, please, retry :(</span>
     </div>
   </form>
