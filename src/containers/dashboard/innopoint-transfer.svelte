2b8ff789baa8233f2b28fb8471a2b5ef65264f32
VanishMax <maxkors2014@gmail.com>
2020-01-03 11:35:09 +0300

    Transfer markup for user operations in admin dashboard

--- src/containers/dashboard/innopoint-transfer.svelte
+++ src/containers/dashboard/innopoint-transfer.svelte
@@ -1,10 +1,19 @@
 <script>
   import Card from 'ui/card.svelte';
+  import AutoComplete from '@/components/projects/new/autocomplete.svelte';
+  import TransferOperations from '@/components/dashboard/transfer-operations.svelte';
 </script>
 
 <Card classname="card individual-ctls">
   <div class="title">
     <svg src="/images/icons/user.svg" class="icon" />
     Individual user controls
+  </div>
+  <div class="panel">
+    <div class="user-selection">
+      <AutoComplete />
+      <p>You may select several people for a certain operation.</p>
+    </div>
+    <TransferOperations />
   </div>
 </Card>
