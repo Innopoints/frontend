cf84aac6d5b4624b0cfc26a15f46f62612093cfb
VanishMax <maxkors2014@gmail.com>
2020-01-05 18:43:53 +0300

    Fix header icons for different types of users

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -18,9 +18,15 @@
       {#if isProfile}
         <Button on:click={signOut}>sign out</Button>
       {:else}
-        <Button href={$user.is_admin ? '/dashboard' : '/profile'} isRound>
-          <svg src="images/icons/user.svg" />
-        </Button>
+        {#if $user.is_admin}
+          <Button href="/dashboard" isRound>
+            <svg src="images/icons/grid.svg" />
+          </Button>
+        {:else}
+          <Button href="/profile" isRound>
+            <svg src="images/icons/user.svg" />
+          </Button>
+        {/if}
       {/if}
     {/if}
   </div>
--- src/containers/home/header.svelte
+++ src/containers/home/header.svelte
@@ -15,9 +15,15 @@
     <div class="actions">
       <!-- TODO: decomment after markup changes -->
       <!--<NotificationCenter />-->
-      <Button href={$user.is_admin ? '/dashboard' : '/profile'} isRound>
-        <svg src="images/icons/user.svg" />
-      </Button>
+      {#if $user.is_admin}
+        <Button href="/dashboard" isRound>
+          <svg src="images/icons/grid.svg" />
+        </Button>
+      {:else}
+        <Button href="/profile" isRound>
+          <svg src="images/icons/user.svg" />
+        </Button>
+      {/if}
     </div>
   {/if}
 </header>
