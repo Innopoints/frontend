73f6a7fcdae41fbd2fb33070a466bf8a75c3fe06
VanishMax <maxkors2014@gmail.com>
2020-01-05 22:49:58 +0300

    Fix request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,11 +4,19 @@ import {API_HOST} from '@/constants/env';
 // in Sapper's preload() function there's this.fetch function that
 // distinguishes between node-fetch and usual fetch depending on env
 export default async (fetchType = fetch, url = '', method = 'GET', body = {}, isForm = false) => {
-  return await fetchType(API_HOST + url, {
+  let options = {
     credentials: 'include',
-    body: isForm ? body : Object.entries(body).length ? JSON.stringify(body) : undefined,
     method,
-  }).then(res => {
+  };
+
+  if (isForm) options.body = body;
+  else if (Object.entries(body).length) options.body = JSON.stringify(body);
+
+  let headers = new Headers();
+  if (!isForm && Object.entries(body).length) headers.append('Content-Type', 'application/json');
+  options.headers = headers;
+
+  return await fetchType(API_HOST + url, options).then(res => {
     if (res.status === 204) return true;
     else if (res.ok) return res.json();
     return null;
