0299eb5d8a547c3d7436e9edffcc4fdf2ff6dcfe
illright <leva181777@gmail.com>
2020-01-09 01:31:08 +0500

    Refactor the Labeled component

--- src/containers/ui/labeleds.svelte
+++ src/containers/ui/labeleds.svelte
@@ -7,16 +7,12 @@
   <h1>Labeled Elements</h1>
   <div class="showcase">
     <Labeled label="Label">
-      <span style="font-size: 1.4rem">1000 innopoints</span>
+      <span>1000 innopoints</span>
     </Labeled>
 
     <Labeled icon label="Label">
-      <svg
-          src="/images/icons/award.svg"
-          class="icon"
-          slot="icon"
-      />
-      <span style="font-size: 1.4rem">1000 innopoints</span>
+      <svg src="/images/icons/award.svg" class="icon" slot="icon" />
+      <span>1000 innopoints</span>
     </Labeled>
   </div>
 </Card>
