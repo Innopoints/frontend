26af5c9f62bf776a0ed54dde144f64d400816a7c
illright <leva181777@gmail.com>
2020-01-09 23:05:50 +0500

    Refactor the chips into CheckChipGroup More chip-related components to come

--- src/containers/ui/chips.svelte
+++ src/containers/ui/chips.svelte
@@ -1,19 +1,20 @@
 <script>
   import Card from 'ui/card.svelte';
 
-  import Chip from 'ui/chip.svelte';
-  import ChipGroup from 'ui/chip-group.svelte';
+  // import Chip from 'ui/chip.svelte';
+  import CheckChipGroup from 'ui/check-chip-group.svelte';
 
-  const chips = [
-    'one',
-    'two',
-    'three',
+  let values = [
+    { value: 1, checked: true },
+    { value: 2, checked: false, disabled: true },
+    { value: 3, checked: false },
   ];
+  const labels = ['one', 'two', 'three'];
 </script>
 
 <Card id="chips">
   <h1>Chips</h1>
-  <div class="showcase">
+  <!-- <div class="showcase">
     <div style="display: flex; flex-wrap: wrap;">
       <Chip disabled>most first</Chip>
       <Chip disabled>least first</Chip>
@@ -21,11 +22,11 @@
       <Chip disabled isOutline>most last</Chip>
     </div>
   </div>
-
+  <hr /> -->
+  <CheckChipGroup bind:values={values} labels={labels} name="chip0" classname="flex-wrap" />
   <hr />
-  <ChipGroup items={chips} value={chips[0]} name="chip-11" classname="flex-wrap" />
-
-  <hr />
+  <CheckChipGroup bind:values={values} name="chip1" classname="flex-wrap" maxChecked={1} />
+  <!--
   <ChipGroup items={chips} value={chips} name="chip-12" multiple classname="flex-wrap" />
 
   <hr />
@@ -49,5 +50,5 @@
       <Chip name="chip-2" multiple>least first</Chip>
       <Chip name="chip-2" multiple>most last</Chip>
     </div>
-  </div>
+  </div> -->
 </Card>
