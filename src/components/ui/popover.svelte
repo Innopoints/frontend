04e89b5844055d49025e7887d0a4b1aa73a177f2
illright <leva181777@gmail.com>
2020-01-10 01:13:44 +0500

    Add the Popover component

---
+++ src/components/ui/popover.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let classname = '';
+  export let popoverclass = '';
+</script>
+
+<div class="popover-container {classname}">
+  <slot name="handle" />
+  <slot name="popover">
+    <div class="popover {popoverclass}">
+      <slot name="popover-content" />
+    </div>
+  </slot>
+</div>
