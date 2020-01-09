5cf63e01a442e3d56a4efe9110b6ed7d3b8c8873
illright <leva181777@gmail.com>
2020-01-09 23:31:13 +0500

    Add the RadioChipGroup and UnclickableChipGroup

---
+++ src/components/ui/radio-chip-group.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let classname = '';
+  export let chipwrapperclass = '';
+  export let chipclass = '';
+  export let inputclass = '';
+  export let small = false;
+  export let outline = false;
+
+  export let values;
+  export let labels = null;
+  export let value = null;
+  export let name;
+
+  if (values.length === 0) {
+    console.error('Must have at least one value in the checkbox group.');
+  }
+
+  if (labels !== null && values.length !== labels.length) {
+    console.error('Must have as many labels as there is values.');
+  }
+</script>
+
+<div role="group" class={classname}>
+  {#each values as loopValue, i (i)}
+    <label class="chip-wrapper {chipwrapperclass}">
+      <input
+        bind:group={value}
+        value={loopValue}
+        type="radio"
+        name={name}
+        class={inputclass}
+      />
+      <div class="chip {chipclass}" class:small class:outline>
+        {labels === null ? loopValue : labels[i]}
+      </div>
+    </label>
+  {/each}
+</div>
