a830f76579b32c53fb47d9a067de9f97cdcb647a
illright <leva181777@gmail.com>
2020-01-10 22:31:29 +0500

    Add the Star component

---
+++ src/containers/ui/star-ratings.svelte
@@ -0,0 +1,15 @@
+<script>
+  import StarRating from 'ui/star-rating.svelte';
+  import Card from 'ui/card.svelte';
+
+  let value = null;
+  let value2 = 3;
+</script>
+
+<Card id="stars-selectable">
+  <h1>Star Ratings</h1>
+  <div class="showcase vertical">
+    <StarRating bind:value name="star-rating1" />
+    <StarRating bind:value={value2} max={10} name="star-rating2" />
+  </div>
+</Card>
