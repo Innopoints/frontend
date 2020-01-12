ee53e6cb8216bc281331a0d5fd28445414275847
illright <leva181777@gmail.com>
2020-01-12 22:11:13 +0500

    Implement the Balance store component

---
+++ src/components/store/balance.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let value = null;
+</script>
+
+{#if value != null}
+  <div class="balance">
+    You have {value}
+    <svg src="images/innopoint-sharp.svg" alt="innopoint{value !== 1 ? 's' : ''}" />
+  </div>
+{/if}
