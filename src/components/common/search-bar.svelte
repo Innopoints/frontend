2ffa8fd02e779adf2f85dea93a20ecf9884fbc92
illright <leva181777@gmail.com>
2020-01-12 22:15:12 +0500

    Implement the SearchBar common component

---
+++ src/components/common/search-bar.svelte
@@ -0,0 +1,10 @@
+<script>
+  import TextField from 'ui/text-field.svelte';
+
+  export let value = '';
+  export let placeholder = 'Type to search';
+</script>
+
+<TextField on:input {placeholder} isWithItem {value}>
+  <svg src="images/icons/search.svg" class="item" />
+</TextField>
