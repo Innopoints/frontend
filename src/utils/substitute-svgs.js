4b978d8fed7bcb343ed5795a1ebdd00fa7cd0411
illright <leva181777@gmail.com>
2020-01-12 18:04:42 +0500

    Refactor the SVG substitution function, keep the old classes

--- src/utils/substitute-svgs.js
+++ src/utils/substitute-svgs.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path, { extname } from 'path';
 
 const selfClosingSvgs = /<svg([^>]*)\/>/gi;
-const selfClosingSvgsLocal = /<svg([^>]*)\/>/gi;
+const selfClosingSvgsLocal = /<svg([^>]*)\/>/i;
 const attributesRegex = /([a-zA-Z-0-9]+)="([^"]*)"/gi;
 const fullSvg = /<svg([^>]*)>([^]*)<\/svg>/i;
 
@@ -11,7 +11,7 @@ function processCustomSvg(svg) {
   const svgMatch = svg.match(selfClosingSvgsLocal);
   const attributes = {};
 
-  for (let attrMatch of svgMatch[0].matchAll(attributesRegex)) {
+  for (let attrMatch of svgMatch[1].matchAll(attributesRegex)) {
     attributes[attrMatch[1]] = attrMatch[2];
   }
 
@@ -21,34 +21,37 @@ function processCustomSvg(svg) {
   }
 
   const pathToSvg = path.join(__dirname, 'static/', attributes.src);
-  if (fs.existsSync(pathToSvg)) {
-    const svgFile = fs.readFileSync(pathToSvg, 'utf8');
-    let [, svgAttrs, svgContent] = svgFile.match(fullSvg) || [];
-    delete attributes.src;
-
-    if ('alt' in attributes) {
-      svgContent = `<title>${attributes.alt}</title>${svgContent}`;
-      delete attributes.alt;
-    }
-
-    if ('class' in attributes) {
-      if (svgAttrs.includes('class="')) {
-        svgAttrs = svgAttrs.replace(/class="[^"]*"/, `class="${attributes.class}"`);
-      } else {
-        svgAttrs += ` class="${attributes.class}"`;
-      }
-      delete attributes.class;
-    }
-
-    for (let leftAttr in attributes) {
-      svgAttrs += ` ${leftAttr}="${attributes[leftAttr]}"`;
-    }
-
-    return `<svg ${svgAttrs.trim()}>${svgContent}</svg>`;
-  } else {
+  if (!fs.existsSync(pathToSvg)) {
     console.error(`Can't locate file '${pathToSvg}'.`);
     return '';
   }
+
+  const svgFile = fs.readFileSync(pathToSvg, 'utf8');
+  let [, svgAttrs, svgContent] = svgFile.match(fullSvg) || [];
+  delete attributes.src;
+
+  if ('alt' in attributes) {
+    svgContent = `<title>${attributes.alt}</title>${svgContent}`;
+    delete attributes.alt;
+  }
+
+  if ('class' in attributes) {
+    if (svgAttrs.includes('class="')) {
+      svgAttrs = svgAttrs.replace(
+        /class="([^"]*)"/,
+        (match) => `class="${attributes.class} ${match.slice(7, -1)}"`,
+      );
+    } else {
+      svgAttrs += ` class="${attributes.class}"`;
+    }
+    delete attributes.class;
+  }
+
+  for (let leftAttr in attributes) {
+    svgAttrs += ` ${leftAttr}="${attributes[leftAttr]}"`;
+  }
+
+  return `<svg ${svgAttrs.trim()}>${svgContent}</svg>`;
 }
 
 export default function substituteSvgs({ content, filename }) {
