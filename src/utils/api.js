3a1a35884859c7c940ba94bb9274686f707235ee
illright <leva181777@gmail.com>
2020-01-13 17:58:38 +0500

    Remove debug console.logs

--- src/utils/api.js
+++ src/utils/api.js
@@ -26,8 +26,6 @@ function request(method, url, data) {
 
   return fetch(url, options)
     .then(r => {
-      console.log(r);
-      console.log(r.status);
       if (r.status == 204) {
         return null;
       } else {
