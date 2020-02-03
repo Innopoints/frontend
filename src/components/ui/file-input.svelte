e3e1232455cf830c5a6f935a7f675337ede6b420
illright <leva181777@gmail.com>
2020-02-03 14:56:13 +0300

    Add a file picker component

---
+++ src/components/ui/file-input.svelte
@@ -0,0 +1,13 @@
+<script>
+  import ripple from 'ui/utils/ripple.js';
+  
+  export let disabled = false;
+</script>
+
+<label class="file-input">
+  <input type="file" on:change {disabled} />
+  <div class="handle" use:ripple>
+    <svg src="/images/icons/image.svg" class="icon mr" />
+    upload cover image
+  </div>
+</label>
