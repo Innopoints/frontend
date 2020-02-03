f645a8548aad1c8d15055256867558171eeca862
illright <leva181777@gmail.com>
2020-02-03 23:04:35 +0300

    Allow constraining the file mimetype

--- src/components/ui/file-input.svelte
+++ src/components/ui/file-input.svelte
@@ -1,12 +1,17 @@
 <script>
   import ripple from 'ui/utils/ripple.js';
-  
+
   export let disabled = false;
+  export let accept = null;
+  let additionalProps = {};
+  if (accept != null) {
+    additionalProps.accept = accept;
+  }
 </script>
 
 <label class="file-input">
-  <input type="file" on:change {disabled} />
   <div class="handle" use:ripple>
+  <input type="file" on:change {disabled} {...additionalProps} />
     <svg src="/images/icons/image.svg" class="icon mr" />
     upload cover image
   </div>
