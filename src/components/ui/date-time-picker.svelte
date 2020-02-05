fb526bdcef7c9d2f6e5a40f61d21b647a8df07a8
illright <leva181777@gmail.com>
2020-02-05 10:09:40 +0300

    Add datetime pickers

---
+++ src/components/ui/date-time-picker.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import Button from './button.svelte';
+  import DatePicker from './date-picker.svelte';
+  import TimePicker from './time-picker.svelte';
+
+  export let classname = '';
+  export let value = null;
+
+  const dispatch = createEventDispatcher();
+
+  let datePickerShown = true;
+  let dateValue = null;
+  let timeValue = { hours: null, minutes: null };
+
+  function combineValues() {
+    if (dateValue != null && timeValue.hours != null) {
+      dateValue.setHours(timeValue.hours);
+      dateValue.setMinutes(timeValue.minutes);
+      value = dateValue;
+      dispatch('change', value);
+    } else {
+      setTimeout(() => datePickerShown = dateValue == null, 10);
+    }
+  }
+
+  function clear() {
+    dateValue = null;
+    timeValue = { hours: null, minutes: null };
+    value = null;
+    datePickerShown = true;
+  }
+</script>
+
+<div class="date-time-picker {classname}">
+  <div class="mode-switch">
+    <Button isSmall isSelected={datePickerShown} on:click={() => datePickerShown = true}>
+      date
+    </Button>
+    <Button isSmall isSelected={!datePickerShown} on:click={() => datePickerShown = false}>
+      time
+    </Button>
+    <Button isDanger isSmall on:click={clear}>
+      clear
+    </Button>
+  </div>
+
+  {#if datePickerShown}
+    <DatePicker bind:value={dateValue} on:change={combineValues} />
+  {:else}
+    <div class="time-wrapper">
+      <svg class="icon" src="images/icons/clock.svg" />
+      <TimePicker bind:value={timeValue} />
+      <Button on:click={combineValues}>
+        <svg class="icon mr" src="images/icons/check.svg" />
+        select
+      </Button>
+    </div>
+  {/if}
+</div>
