e73daa5736e67f2c1dfb2f6990c24a6d4dc397e0
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-03-29 23:13:25 +0300

    Create module to manage subscribing to push

---
+++ src/utils/notifications-subscribe.js
@@ -0,0 +1,45 @@
+import * as api from './api.js';
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+let unsub; 
+
+export default async function subscribe() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+  const registration = await navigator.serviceWorker.getRegistration();
+  if (registration == undefined) {
+    return;
+  }
+  const pushSubscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array('BLHdmxCmBlgSj8NHRwRs7IqWD1ucN_aZuk2aEdFGjGLDAreYnlNUi-MGxuMvvdrYKSLNESQHN5dxo2isFxfdfp4'),
+  });
+  console.log('Received notification subscription:', pushSubscription);
+  unsub = pushSubscription.unsubscribe.bind(pushSubscription);
+  api.post('/notifications/subscribe', {
+    data: pushSubscription,
+  });
+}
+
+export function unsubscribe() {
+  if (!unsub) {
+    console.error('No subscription to remove');
+    return;
+  }
+  unsub();
+}
