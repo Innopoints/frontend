2531e350d0bb03a12bb1437ee388adfd46d28944
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-03-30 15:29:41 +0300

    register the service worker

--- rollup.config.js
+++ rollup.config.js
@@ -121,4 +121,20 @@ export default {
 
     onwarn,
   },
+
+  serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode),
+			}),
+			commonjs(),
+			!dev && terser(),
+		],
+
+		onwarn,
+	},
 };
