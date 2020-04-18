c8dc85443930621b588a80725bcf9f063c78f62e
illright <leva181777@gmail.com>
2020-04-19 00:21:21 +0300

    Add the cropping modal

---
+++ src/components/common/image-resizer.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import Button from 'ui/button.svelte';
+  import Dialog from 'ui/dialog.svelte';
+  import Modal from 'ui/modal.svelte';
+  import Cropper from 'svelte-easy-crop';
+
+  export let image;
+  let crop = { x: 0, y: 0 };
+  let pixels = null;
+  export let aspectRatio = 1;
+  export let isOpen = false;
+  export let error = null;
+
+  function saveBounds(event) {
+    pixels = event.detail.pixels;
+  }
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<style>
+  .image-resizer {
+    position: relative;
+    width: 15em;
+    border-radius: .5em;
+    overflow: hidden;
+    height: 9em;
+  }
+
+  @media only screen and (min-width: 360px) {
+    .image-resizer {
+      width: 18em;
+      height: 10em;
+    }
+  }
+
+  @media only screen and (min-width: 640px) {
+    .image-resizer {
+      width: 32em;
+      height: 18em;
+    }
+  }
+</style>
+
+<Modal bind:isOpen>
+  {#if image != null}
+    <Dialog
+      title="Resize the image"
+      closeCallback={() => isOpen = false}
+    >
+      <div class="image-resizer" slot="content">
+        <Cropper {image} {crop} aspect={aspectRatio} on:cropcomplete={saveBounds} />
+      </div>
+      <div class="actions" slot="content">
+        {#if error != null}
+          {error}
+        {/if}
+        <Button isDanger on:click={() => isOpen = false}>cancel</Button>
+        <Button
+          isFilled
+          on:click={() => dispatch('image-cropped', pixels)}
+        >
+          upload
+        </Button>
+      </div>
+    </Dialog>
+  {/if}
+</Modal>
--- src/components/projects/new/project-image-picker.svelte
+++ src/components/projects/new/project-image-picker.svelte
@@ -1,51 +1,28 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   import Button from 'ui/button.svelte';
   import FileInput from 'ui/file-input.svelte';
-  import * as api from '@/utils/api.js';
   import { API_HOST_BROWSER } from '@/constants/env.js';
   import maxSizeMB from '@/constants/backend/file-upload-limit.js';
 
   export let value = null;
-  let promise = null;
-  let blobURL = null;
   let error = null;
 
-  async function handleImageUpload(event) {
-    let file = event.target.files[0];
+  function validateUpload(event) {
+    const file = event.target.files[0];
     if (file.size > maxSizeMB * 1024 * 1024) {
       error = `Selected file is too large (${maxSizeMB} MB max).`;
       return;
     }
-
-    blobURL = URL.createObjectURL(file, { type: file.type });
-    const formData = new FormData();
-    formData.append('file', file);
-
-    promise = api.post('/file', {
-      data: formData,
-    });
-
-    try {
-      const resp = await promise;
-      if (resp.ok) {
-        value = (await resp.json()).id;
-        error = null;
-      } else {
-        console.error(await resp.text());
-        error = 'Upload failed. Try again.';
-      }
-    } catch (e) {
-      console.error(e);
-      error = 'Upload failed. Try again.';
-    }
+    dispatch('resize-image', file);
   }
 
   function clearValue() {
     value = null;
-    promise = null;
-    blobURL = null;
     error = null;
   }
+
+  const dispatch = createEventDispatcher();
 </script>
 
 {#if value != null}
@@ -54,29 +31,9 @@
     <svg src="images/icons/trash-2.svg" class="icon" />
     <span class="text">delete</span>
   </Button>
-{:else if promise != null}
-  {#await promise.then(resp => resp.json()).catch(() => promise = null)}
-    <div class="loading-wrapper shadow-1">
-      <img
-        src={blobURL || '/images/create-project/placeholder.svg'}
-        alt="Project image"
-      />
-      <div class="status-overlay">
-        <div class="lds-ellipsis">
-          <div></div><div></div><div></div><div></div>
-        </div>
-      </div>
-    </div>
-  {:then image}
-    <img src="{API_HOST_BROWSER}/file/{image.id}" alt="Project image" class="shadow-1 mr" />
-    <Button isDanger on:click={clearValue} tooltip="Delete image">
-      <svg src="images/icons/trash-2.svg" class="icon" />
-      <span class="text">delete</span>
-    </Button>
-  {/await}
 {:else}
   <div class="options">
-    <FileInput on:change={handleImageUpload} accept="image/*" />
+    <FileInput on:change={validateUpload} accept="image/*" />
     {#if error}
       <p class="error">{error}</p>
     {/if}
