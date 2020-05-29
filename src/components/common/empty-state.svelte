10b1279cf208053e809d62cb3ef0ef0dbe0d0019
illright <leva181777@gmail.com>
2020-05-29 21:28:17 +0300

    Create a component for the empty state

---
+++ src/components/common/empty-state.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let text = null;
+  export let small = false;
+</script>
+
+<div class="empty" class:small>
+  <div class="icon">
+    <slot />
+  </div>
+  {#if text != null}
+    <p class="title">{text}</p>
+  {/if}
+</div>
+
+<style src="../../../static/css/components/common/empty-state.scss"></style>
