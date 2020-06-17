4eb19be770714edba8621cc3d61a0c3ac369f2ca
illright <leva181777@gmail.com>
2020-06-17 21:01:07 +0300

    Fix up the two-state switch

--- src/components/ui/two-state-switch.svelte
+++ src/components/ui/two-state-switch.svelte
@@ -1,44 +1,45 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { Switch } from 'attractions';
 
-  export let classname = '';
-  export let inputclass = '';
-  export let knobclass = '';
-  export let switchclass = '';
-  export let firstclass = '';
-  export let secondclass = '';
-
-  export let name = '';
-  export let first = '';
-  export let second = '';
+  export let name;
+  export let first;
+  export let second;
   export let value = first;
   $: value = (boolValue ? second : first);
   export let boolValue = false;
-
-  const dispatch = createEventDispatcher();
-  function onChange(evt) {
-    boolValue = this.checked;
-    dispatch('change', evt);
-  }
 </script>
 
-<label class="clickable switch-wrapper {classname}">
-  <input
-    on:change={onChange}
-    checked={boolValue}
-    name={name}
-    on:focus
-    class="switch-ctl {inputclass}"
-    type="checkbox"
-  />
+<div class="two-state-switch">
+  <span class="state first" class:selected={!boolValue}>{first}</span>
+  <Switch {name} bind:value={boolValue} />
+  <span class="state second" class:selected={boolValue}>{second}</span>
+</div>
 
-  <slot name="first-state">
-    <span class="state first {firstclass}">{first}</span>
-  </slot>
-  <div class="two-state switch {switchclass}">
-    <div class="knob {knobclass}" />
-  </div>
-  <slot name="second-state">
-    <span class="state second {secondclass}">{second}</span>
-  </slot>
-</label>
+<style lang="scss">
+  @import "_attractions-theme.scss";
+
+  .two-state-switch {
+    display: flex;
+    align-items: center;
+
+    :global {
+      .track {
+        border-color: $main !important;
+      }
+
+      .thumb {
+        background-color: $main !important;
+      }
+    }
+  }
+
+  .state {
+    font-weight: 500;
+    margin: 0 .8em;
+    color: $off-state;
+
+    &.selected {
+      color: $main;
+    }
+  }
+</style>
