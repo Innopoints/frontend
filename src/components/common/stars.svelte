2b7099bf5eee7e9fa0ef7645b7f18d84bd31d2fe
illright <leva181777@gmail.com>
2020-06-19 21:24:36 +0300

    Add the Stars component

---
+++ src/components/common/stars.svelte
@@ -0,0 +1,18 @@
+<script>
+  import range from '@/utils/range.js';
+
+  export let value;
+  export let max = 5;
+</script>
+
+<div class="stars">
+  {#each [...range(max)] as i}
+    {#if i < value}
+      <svg class="star" src="images/icons/star.svg" />
+    {:else}
+      <svg class="star off" src="images/icons/star.svg" />
+    {/if}
+  {/each}
+</div>
+
+<style src="../../../static/css/components/common/stars.scss"></style>
