2655959fd3dfe13bc5cb5d5c0f782e514c201223
illright <leva181777@gmail.com>
2020-06-24 20:30:41 +0300

    Restore the Edit Project page

--- src/routes/projects/[id([0-9]+)]/edit.svelte
+++ src/routes/projects/[id([0-9]+)]/edit.svelte
@@ -1,5 +1,7 @@
 <script context="module">
+  import { writable } from 'svelte/store';
   import getInitialData from '@/utils/get-initial-data.js';
+  import { prepareAfterBackend } from '@/utils/project-manipulation.js';
 
   export async function preload(page, session) {
     const data = await getInitialData(this, session, new Map([
@@ -8,11 +10,13 @@
 
     if (session.account == null
         || (session.account.email !== data.project.creator.email
-            && session.account.is_admin)) {
+            && !session.account.is_admin)) {
       this.error(403, 'Edit the Project');
     }
 
+    data.project.activities.forEach(prepareAfterBackend);
     data.account = session.account;
+    data.project = writable(data.project);
     return data;
   }
 </script>
@@ -20,180 +24,148 @@
 <script>
   import { goto } from '@sapper/app';
   import Layout from '@/layouts/default.svelte';
+  import {
+    Autocomplete,
+    Button,
+    Divider,
+    Dropdown,
+    DropdownShell,
+    FormField,
+    H1,
+    Label,
+    SnackbarContainer,
+    TextField,
+  } from 'attractions';
+  import { SnackbarPositions } from 'attractions/snackbar';
   import ProjectImagePicker from '@/components/projects/new/project-image-picker.svelte';
-  import FormField from 'ui/form-field.svelte';
-  import Button from 'ui/button.svelte';
-  import TextField from 'ui/text-field.svelte';
-  import Autocomplete from 'ui/autocomplete.svelte';
-  import generateQueryString from '@/utils/generate-query-string.js';
   import * as api from '@/utils/api.js';
   import spaceOnly from '@/utils/space-only.js';
+  import {
+    autocompleteValueToUser,
+    getUsersRaw,
+    minSearchLength,
+    userToAutocompleteValue,
+  } from '@/utils/autocomplete-users.js';
 
   export let account;
   export let project;
 
-  let duplicateName = false;
+  let snackbarContainer = null;
+  let moderatorsValue = $project.moderators.filter(notCreator).map(userToAutocompleteValue);
 
-  function userToAutocompleteValue(user) {
-    return {
-      name: user.full_name,
-      details: user.email,
-    };
+  export function notCreator(moderator) {
+    return moderator.email !== account.email;
   }
-
-  function notCreator(moderator) {
-    return moderator.email !== project.creator.email;
-  }
-
-  project.moderators = project.moderators.filter(notCreator);
-  let values = project.moderators.map(userToAutocompleteValue);
 
   function recordChanges() {
-    project.moderators = values.map(user => ({
-      full_name: user.name,
-      email: user.details,
-    }));
+    $project.moderators = moderatorsValue.map(autocompleteValueToUser);
   }
 
-  async function *getUsers(query) {
-    if (query.length < 3) {
+  async function* getUsers(query) {
+    try {
+      yield* getUsersRaw(query, notCreator);
+    } catch (e) {
+      snackbarContainer.showSnackbar({
+        props: {
+          text: 'Couldn\'t search for users, try reloading the page',
+        },
+      });
+      console.error(e);
       return [];
-    }
-
-    const initialResp = await api.get(`/accounts?${generateQueryString(new Map([['q', query]]))}`);
-    if (!initialResp.ok) {
-      if (initialResp.status === 400) {
-        console.error(await initialResp.json());
-      } else {
-        console.error(await initialResp.text());
-      }
-      return [];
-    }
-
-    let { pages, data } = await initialResp.json();
-    let currentPage = 1;
-    if (currentPage >= pages) {
-      return data.filter(notCreator).map(userToAutocompleteValue);
-    }
-
-    yield data.filter(notCreator).map(userToAutocompleteValue);
-
-    let resp;
-    while (currentPage <= pages) {
-      let queryString = generateQueryString(new Map([['q', query], ['page', currentPage]]));
-      resp = await api.get(`/accounts?${queryString}`);
-      if (!resp.ok) {
-        if (resp.status === 400) {
-          console.error(await resp.json());
-        } else {
-          console.error(await resp.text());
-        }
-        return [];
-      }
-
-      let { data, pages } = await resp.json();
-      if (currentPage === pages) {
-        return data.filter(notCreator).map(userToAutocompleteValue);
-      } else {
-        yield data.filter(notCreator).map(userToAutocompleteValue);
-      }
-      currentPage++;
     }
   }
 
   async function saveChanges() {
     try {
-      await api.json(api.patch(`/projects/${project.id}`, {
+      await api.json(api.patch(`/projects/${$project.id}`, {
         data: {
-          name: project.name,
-          image_id: project.image_id,
-          moderators: project.moderators,
+          name: $project.name,
+          image_id: $project.image_id,
+          moderators: $project.moderators,
         },
         csrfToken: account.csrf_token,
       }));
-      goto(`/projects/${project.id}`);
+      goto(`/projects/${$project.id}`);
     } catch (e) {
+      snackbarContainer.showSnackbar({
+        props: {
+          text: 'Couldn\'t save the changes, try reloading the page',
+        },
+      });
       console.error(e);
     }
   }
 </script>
 
 <svelte:head>
-  <title>Edit {project.name} – Innopoints</title>
-
-  <link rel="stylesheet" href="/css/bundles/projects-id-edit.min.css" />
-  <link rel="prefetch" as="style" href="/css/bundles/projects-id.min.css" />
-  {#if account}
-    {#if account.is_admin}
-      <link rel="prefetch" as="style" href="/css/bundles/dashboard.min.css" />
-    {:else}
-      <link rel="prefetch" as="style" href="/css/bundles/profile.min.css" />
-    {/if}
-  {/if}
+  <title>Edit {$project.name} – Innopoints</title>
 </svelte:head>
 
 <Layout user={account}>
-  <div class="material step1">
-    <form class="padded">
-      <h1>
-        Edit {project.name}
-      </h1>
+  <SnackbarContainer position={SnackbarPositions.BOTTOM_LEFT} bind:this={snackbarContainer}>
+    <div class="material padded">
+      <H1>Edit {$project.name}</H1>
       <FormField
-        title="Project name"
-        id="name"
+        name="Project name"
+        help="Prefer English names."
         required
-        error={
-             (duplicateName && "The name must be unique.")
-          || (spaceOnly(project.name) && "The name must not be empty.")
-          || null
-        }
+        errors={[spaceOnly($project.name) && "The name must not be empty."]}
       >
-        <span slot="subtitle" class="desc">
-          <span class="lb">This has to be different from other projects.</span>
-          <span class="lb">Adding the year to the name usually helps:</span><br>
-          <em>“Halloween” → “Halloween 2019”</em>
-        </span>
         <TextField
-          id="title"
+          id="name"
+          autocomplete="off"
           maxlength={128}
-          value={project.name}
-          on:change={(event) => project.name = event.detail}
+          bind:value={$project.name}
         />
       </FormField>
 
       <FormField
-        title="Cover image"
-        id="image"
-        wrapperclass="image-picker"
+        name="Cover image"
+        help="This is shown on the project card to catch attention."
       >
-        <span slot="subtitle" class="desc">
-          <span class="lb">This is shown on the project card to catch attention.</span>
-          <span class="lb">Best to use 16:9 photos.</span>
-        </span>
-        <ProjectImagePicker bind:value={project.image_id} />
+        <ProjectImagePicker bind:value={$project.image_id} />
       </FormField>
 
-      <hr />
+      <Divider />
 
-      <div class="step3">
-        <p>
-          Moderators can process applications from volunteers and edit activities.
-          Feedback on volunteers is also shared between all moderators.
-        </p>
-        <p>
-          You are the creator, therefore, also a moderator.
-        </p>
-        <div class="label">Search for people</div>
-        <Autocomplete bind:selection={values} getOptions={getUsers} on:change={recordChanges} />
+      <div class="instructions">
+        <p>This step is <strong>optional</strong>.</p>
+        <DropdownShell let:toggle>
+          <Button on:click={toggle}>
+            <svg src="images/icons/help-circle.svg" class="mr" />
+            who are moderators?
+            <svg src="images/icons/chevron-down.svg" class="ml dropdown-chevron" />
+          </Button>
+          <Dropdown>
+            <p>Moderators can process applications from volunteers and edit activities.</p>
+            <p>Reports and ratings on volunteers are also shared between all moderators.</p>
+            <p>You are a moderator, too, by the way, automatically.</p>
+          </Dropdown>
+        </DropdownShell>
       </div>
-      <div class="actions padded">
-        <Button isDanger href="/projects/{project.id}">
+
+      <Label class="mb">Search for other people</Label>
+      <Autocomplete
+        bind:selection={moderatorsValue}
+        getOptions={getUsers}
+        placeholder="Start typing a name or an e-mail..."
+        on:change={recordChanges}
+        {minSearchLength}
+      />
+      <p>
+        Can't find who you're looking for?
+        Make sure they have logged into this website at least once.
+      </p>
+      <div class="actions">
+        <Button danger class="mr" href="/projects/{project.id}">
           discard changes
         </Button>
-        <Button isFilled on:click={saveChanges}>
+        <Button filled on:click={saveChanges}>
           save changes
         </Button>
       </div>
-    </form>
-  </div>
+    </div>
+  </SnackbarContainer>
 </Layout>
+
+<style src="../../../../static/css/routes/projects/view/edit.scss"></style>
