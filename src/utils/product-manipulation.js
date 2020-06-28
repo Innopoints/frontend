c769a10835010c8d1a812d69710cb1235e53add9
illright <leva181777@gmail.com>
2020-06-28 19:02:32 +0300

    Restore the Edit Product page

--- src/utils/product-manipulation.js
+++ src/utils/product-manipulation.js
@@ -1,3 +1,6 @@
+import arraysEqual from '@/utils/arrays-equal.js';
+import setNullSafe from '@/utils/set-null-safe.js';
+
 /* Returns a map of colors to arrays of varieties. */
 export function groupByColor(varieties) {
   const grouping = new Map();
@@ -22,4 +25,88 @@ export function groupByID(varieties) {
   }
 
   return grouping;
+}
+
+/* Convert the backend varieties into human varieties. */
+export function prepareAfterBackend(product) {
+  const varietiesByColor = groupByColor(product.varieties);
+
+  product.sized = product.varieties[0].size != null;
+  product.varieties = [];
+  for (let color of varietiesByColor.keys()) {
+    let varieties = varietiesByColor.get(color);
+    let thisColorVariety = Object.assign({}, varieties[0]);
+
+    if (product.sized) {
+      thisColorVariety.sizes = {};
+      for (let variety of varieties) {
+        thisColorVariety.sizes[variety.size] = variety.amount;
+      }
+      thisColorVariety.quantity = 0;
+    } else {
+      thisColorVariety.quantity = thisColorVariety.amount;
+      thisColorVariety.sizes = {};
+    }
+
+    delete thisColorVariety.size;
+    delete thisColorVariety.amount;
+    delete thisColorVariety.purchases;
+    product.varieties.push(thisColorVariety);
+  }
+
+  return product;
+}
+
+/* Compute differences between two products in four categories:
+   - modified fields
+   - new varieties
+   - modified varieties
+   - deleted varieties */
+export function computeDiffs(product, reference) {
+  reference = copyProduct(reference);
+  const diffs = {
+    fields: null,
+    newVarieties: [],
+    modifiedVarieties: [],
+    deletedVarieties: null,
+  };
+
+  for (let field of ['name', 'type', 'description', 'price']) {
+    if (product[field] !== reference[field]) {
+      diffs.fields = setNullSafe(diffs.fields, field, product[field]);
+    }
+  }
+
+  for (let variety of product.varieties) {
+    const corresponding = reference.varieties.findIndex(
+      candidate => candidate.color == variety.color && candidate.size == variety.size,
+    );
+
+    if (corresponding === -1) {
+      diffs.newVarieties.push(variety);
+    } else {
+      if (!arraysEqual(variety.images, reference.varieties[corresponding].images)
+          || variety.amount !== reference.varieties[corresponding].amount) {
+        variety.id = reference.varieties[corresponding].id;
+        diffs.modifiedVarieties.push(variety);
+      }
+      reference.varieties.splice(corresponding, 1);
+    }
+  }
+  diffs.deletedVarieties = reference.varieties;
+
+  return diffs;
+}
+
+/* Copy a product object, deep-copying nested fields. */
+export function copyProduct(product) {
+  const copy = Object.assign({}, product);
+
+  copy.varieties = product.varieties.map(variety => {
+    const varietyCopy = Object.assign({}, variety);
+    varietyCopy.images = variety.images.slice();
+    return varietyCopy;
+  });
+
+  return copy;
 }
