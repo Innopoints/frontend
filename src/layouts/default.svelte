22ad8610133aabba7433d72ba8a8d12256bf7e0c
illright <leva181777@gmail.com>
2020-06-28 19:47:51 +0300

    Perform random cleanup

--- src/layouts/default.svelte
+++ src/layouts/default.svelte
@@ -2,10 +2,10 @@
   import { stores } from '@sapper/app';
   import Header from '@/components/header.svelte';
   import Footer from '@/components/footer.svelte';
-  export let user = null;
 
   const { page } = stores();
 
+  export let user = null;
   export let headerClass = null;
 </script>
 
