d70b7b79442f8eb99faf228bfe8cfce3fb300aa5
illright <leva181777@gmail.com>
2020-06-29 21:23:58 +0300

    Rename a component to be consistent with the filename

--- src/containers/home/options.svelte
+++ src/containers/home/options.svelte
@@ -1,12 +1,12 @@
 <script>
   import options from 'src/constants/home/options.js';
-  import Step from 'src/components/home/option-step.svelte';
+  import OptionStep from 'src/components/home/option-step.svelte';
 </script>
 
 <section class="options">
   <h2>How to get innopoints?</h2>
   {#each options as option (option.title)}
-    <Step {...option} />
+    <OptionStep {...option} />
   {/each}
 </section>
 
