faa6170de69975b7bc43b8364de5de3722dd2247
illright <leva181777@gmail.com>
2020-06-29 21:32:36 +0300

    Turn arrow functions into proper functions

--- src/utils/optimal-color.js
+++ src/utils/optimal-color.js
@@ -1,34 +1,33 @@
-const trimHEX = (color) => {
+function trimHex(color) {
   if (!color) return '';
   let unhashed = color.replace(/[^0-9a-f]/gi, '');
   if (unhashed.length === 3)
     return unhashed.split('').map(x => x.repeat(2)).join('');
   return unhashed;
-};
+}
 
-const transform = (color) => {
+function transform(color) {
   color /= 255;
   if (color <= 0.03928) {
     return color / 12.92;
   } else {
     return Math.pow((color + 0.055) / 1.055, 2.4);
   }
-};
+}
 
-const luminance = ([r, g, b]) => {
+function luminance([r, g, b]) {
   return transform(r) * 0.2126 + transform(g) * 0.7152 + transform(b) * 0.0722;
-};
+}
 
 // Returns the contrast between the background and content colors,
 // specified as [r, g, b] arrays of numbers in range [0; 255].
-const getContrast = (background, content) => {
+function getContrast(background, content) {
   return (luminance(background) + 0.05) / (luminance(content) + 0.05);
-};
-
+}
 
 // Converts the RGB color ([r, g, b] array of numbers in range [0; 255])
 // to the HSL color ([h, s, l] array of floats in range [0; 1]).
-const rgbToHsl = ([r, g, b]) => {
+function rgbToHsl([r, g, b]) {
   r /= 255; g /= 255; b /= 255;
 
   let maxComp = Math.max(r, g, b);
@@ -52,13 +51,15 @@ const rgbToHsl = ([r, g, b]) => {
   }
 
   return [h, s, l];
-};
+}
 
 // Returns the optimal background color for the color given as
 // a hex string without the hash.
-export default (color) => {
-  color = trimHEX(color);
-  if (!color) return null;
+export default function getBackground(color) {
+  color = trimHex(color);
+  if (!color) {
+    return null;
+  }
 
   let r = parseInt(color.slice(0, 2), 16);
   let g = parseInt(color.slice(2, 4), 16);
@@ -69,4 +70,4 @@ export default (color) => {
   let bgL = (contrast > 1.08 ? 0.97 : 0.96);
 
   return `background: hsl(${Math.floor(h * 360)}, ${s * 100}%, ${bgL * 100}%);`;
-};
+}
