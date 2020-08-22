66d2088d6ab742197dfc9b5e7effde2760b85f0e
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-08-22 21:43:23 +0300

    Revert "Use balance from $session.account"
    This reverts commit db069a81964f3c9da3b451c86779df89ad5f4d8b.

--- src/containers/profile/info.svelte
+++ src/containers/profile/info.svelte
@@ -98,7 +98,7 @@
 
   <div class="balance">
     <p class="btn-align">
-      <span>{$session.account.balance}</span>
+      <span>{account.balance}</span>
       <svg src="static/images/innopoint-sharp.svg" class="innopoint" />
       available
     </p>
